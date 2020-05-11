ece7772374b73afbee180914438f14b509732f91
imxseraph <fmx1992@gmail.com>
2020-05-12 00:35:17 +0800

    update chronos

--- chronos/src/App.js
+++ chronos/src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">欢迎来到轻语树洞，在这里可以与其他人分享你内心的秘密。</h1>
+          <h3>你所留下的话，不带有任何可追踪的特征，本站负责为你保管这个秘密7天，期间可以被他人看到，随后彻底消失。</h3>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
